8c7aa4f6a5bdb562bcb9f31d882722b826512978
AbdulBeard <abdulkhaliqzaheer@gmail.com>
2018-10-08 09:51:55 -0400

    fixing up build stuff

--- buildScript.ps1
+++ buildScript.ps1
@@ -1,13 +1,13 @@
 #npm install -g uglifyjs
-ng build;
+ng build --configuration=azure;
 
 cd dist;
 
-uglifyjs .\zahela-manage\vendor.js -o .\zahela-manage\vendor.js -c -m;
-uglifyjs .\zahela-manage\main.js -o .\zahela-manage\main.js -c -m;
-uglifyjs .\zahela-manage\styles.js -o .\zahela-manage\styles.js -c -m;
+#uglifyjs .\zahela-manage\vendor.js -o .\zahela-manage\vendor.js -c -m;
+#uglifyjs .\zahela-manage\main.js -o .\zahela-manage\main.js -c -m;
+#uglifyjs .\zahela-manage\styles.js -o .\zahela-manage\styles.js -c -m;
 #uglifyjs .\zahela-manage\scripts.js -o .\zahela-manage\scripts.js -c -m;
-uglifyjs .\zahela-manage\polyfills.js -o .\zahela-manage\polyfills.js -c -m;
+#uglifyjs .\zahela-manage\polyfills.js -o .\zahela-manage\polyfills.js -c -m;
 
 $source=".\zahela-manage"
 $exclude = ("*.map");
